a58ee7ea5dff4fa2651a61c8c80ff9374da20bfd
Willandru <willandru@hotmail.com>
2024-07-30 10:53:00 -0500

    Normal multivariada y distancia de malahanollis :p

---
+++ Semana 2/N-bivariada.R
@@ -0,0 +1,27 @@
+## Densidad de na -- NORMAL BIVARIADA -- 
+
+require(mvtnorm)
+
+mu <- c(0,0)
+
+Sigma <- matrix(c(2,0,0,1), ncol=2)
+
+x1<- seq(-5,5,length=40)
+x2<-x1
+
+
+# densidad de la f normal multiv
+
+f<-matrix(0, nrow=length(x1), ncol = length(x2))
+
+for(i in 1:length(x1))
+  for(j in 1:length(x2))
+    f[i,j]<- dmvnorm(c(x1[i], x2[j]), mean=mu, sigma= Sigma)
+
+#--- Grafico
+
+persp(x1, x2, f,
+      theta=70, #ángulo de visualización
+      phi =30,  #ángulo de visualización
+      col="gray",
+      ticktype= "detailed")
